7bd8f585cc198065b01b1be6c760ee8cb4de4541
LorenzoDeBiasi <77537276+El-Febo@users.noreply.github.com>
2023-09-30 12:48:35 +0000

    Commit

--- codes/simboli_e_operatori.cpp
+++ codes/simboli_e_operatori.cpp
@@ -3,3 +3,6 @@
 //[]
 //@
 // res = bool ? a : b     res assume il valore di a se bool è vero, altrimenti di b
+
+//appunto: se invocata una ricorsione, i pezzi di codice successivi alla ricorsione
+//vengono eseguiti.
