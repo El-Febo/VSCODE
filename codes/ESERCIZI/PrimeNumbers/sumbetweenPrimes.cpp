96cf0a261ee05b60757478fef06d8dd03e75d02d
DeBiasi Lorenzo (ACC0172349) <lorenzo.debiasi-1@sci-b106pc054x.unitn.it>
2023-10-05 12:03:20 +0200

    primes

---
+++ codes/ESERCIZI/PrimeNumbers/sumbetweenPrimes.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n; cin >> n;
+
+    for (int i = 2; i < (n/2); i++){
+        //usa una funzione che dica se il numero è primo o meno
+    }
+}
